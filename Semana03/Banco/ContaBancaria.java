ce5e3b061337c76293b332aeda91e3c1e72c9acf
Vinicius <fritzenvinicius@gmail.com>
2025-03-21 21:14:28 -0300

    Semana 03 - parte ii

--- Semana03/Banco/ContaBancaria.java
+++ Semana03/Banco/ContaBancaria.java
@@ -1,4 +1,8 @@
 package Banco;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContaBancaria {
     /*
      *    - NOME 
@@ -12,4 +16,41 @@ public class ContaBancaria {
      *     + EXTRATO
      *     + TOSTRING
      */
+
+    private String nome;
+    private String cpf;
+    private float saldo;
+    private List<String> transacoes;
+
+    public ContaBancaria(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.saldo = 0;
+        this.transacoes = new ArrayList<String>();
+    }
+
+    public void depositar(float valor) {
+        this.saldo = this.saldo + valor;
+        transacoes.add("DEPOSITO NO VALOR DE R$ " + valor);
+    }
+
+    public void sacar(float valor) throws Exception {
+        if (valor > this.saldo) {
+            throw new Exception("SALDO INSUFICIENTE");
+        }
+        this.saldo = this.saldo - valor;
+        this.transacoes.add("SAQUE FEITO NO VALOR DE "  + valor);
+    }
+
+    public float getSaldo() {
+        return this.saldo;
+    }
+
+    public String extrato() {
+        String texto = "\n\n=== EXTRATO ====\n"+ this.nome + "\nSALDO " + saldo;
+        for (String string : transacoes) {
+            texto += "\n" + string;
+        }
+        return texto;
+    }
 }
--- Semana03/Banco/Main.java
+++ Semana03/Banco/Main.java
@@ -2,6 +2,29 @@ package Banco;
 
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria conta = new ContaBancaria();
+        ContaBancaria conta = new ContaBancaria("vini", "123");
+        System.out.println("DEPOSITO DE 1000");
+        conta.depositar(1000);
+        conta.depositar(500);
+        try {
+            System.out.println("SAQUE IMPOSSIVEL");
+            conta.sacar(100000);
+        } catch (Exception e) {
+            System.out.println("ERRO AO SACAR... ");
+            System.out.println(e.getMessage());
+        }
+        
+        try {
+            System.out.println("SAQUE DE 300");
+            conta.sacar(300);
+        } catch (Exception e) {
+            System.out.println("ERRO AO SACAR... ");
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("BUSCA SALDO");
+        System.out.println(conta.getSaldo());
+
+        System.out.println(conta.extrato());
     }
 }
